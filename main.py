484616d4ce2fc36997ea60f19e426c343c7d1d84
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-07 16:30:04 +0530

    Comments,Variables,Datatypes

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("Starting My Python Language Learning Journey!")
+#Comments
+# 1)Single-line Comment
+
+#print heynthere all
+
+
+# 2)Multi-line Commnets
+"""
+DDANKJDODALD
+ADJAFJF
+AJKBFJSU"""
+
+
+
+#Variables:-Used for data storage purpose
+
+a = 13
+b = 15
+
+#Naming Conventions in Variables
+
+camelCase = 1
+PascalCase = 2
+snake_case = 3
+
+
+#DataTypes
+# 1) Number
+
+a = 1
+b = 4.5
+
+# 2) String 
+
+a = "SmartPhones"
+b = 'Welcome!'
+
+# 3) Boolean
+
+a = True
+b = False
+
