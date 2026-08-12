3f083d5b3c9aacd8f91a27352a7da1e101b27aa1
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-12 22:12:09 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,15 @@
+#Function Definition
+def Greet():
+    return "Good to see you here!"
+
+print(Greet())        #Function call
+
+
+
+#Parameters and Arguments
+
+def add(a,b):      #Parameters
+    return a+b
+
+print(add(1,3))    #Arguments
+print(add(b = 13,a = 2))   #Keyword Arguments
