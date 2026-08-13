b5823b4c4938d998cb2882a12d595ef05f6e9eb2
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-13 16:49:20 +0530

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,27 @@
+#Note: Tuple has the heterogenous nature
+
+a = (1,2,3,4,5,"hello")        #Tuple Creation
+print(a[5])                    #Accessing the Tuple
+
+#Note : Tuple is immutable in nature
+#        Tuple can store duplicate values
+
+
+
+#Tuple unpacking
+
+a,b,c = (1,2,3)
+print(c)
+
+
+#Listing elements inside the Tuple
+
+a = [3,2,1]
+tup=tuple(a)        #Similar to type-conversion 
+
+print(tup)
+
+
+
+# Note: Traversing in the List based on Indexing or the value based(Similar to the List Traversing)
+
