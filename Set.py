62a5da6bba6c432a77820da7768d4ec63dffeffd
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-13 21:47:52 +0530

    Set

---
+++ Set.py
@@ -0,0 +1,16 @@
+s = {}
+print(type(s))       #Act as Dictionary
+
+
+k = {1,10,2}
+print(type(k))       #Act as Set
+
+#Traversing in the Set
+
+a = {1,4,5,35,78,3}
+
+for i in a:
+    print(a)
+    break        #Output shows on the basis of Hash-values
+
+# We can modify the set by using it's Inbuilt methods.
