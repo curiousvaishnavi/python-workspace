b753f0e534e3b513f338fee41a5279ee78849745
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-14 23:24:43 +0530

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+
+#Note: Data stored in the form of Key-Value pairs.
+#     Mutable in nature(But we can change only values not the keys)
+
+# Dictionaries can allow only duplicate values but not the duplicate keys(unique)
+
+
+d = {
+    1:10,
+    2:1,
+    "Hello":300            #Dictionary Creation
+}
+
+
+print(d["Hello"])           #Dictionary Accessing(1 is the key)
