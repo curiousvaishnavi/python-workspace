cee61c73ce85932c878efc92edc1be50c8a08d27
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-16 19:49:18 +0530

    Central Tendency

---
+++ CentralTend.py
@@ -0,0 +1,43 @@
+
+#Mean := Average
+
+Mean = Sum of all values / No. of total values
+
+data = 10,20,30,40,50
+Mean = Sum of all values / No. of total values
+Mean = (10 + 20 + 30 + 40 + 50)/5
+Mean = 30
+
+#Median := Middle value of the data, when we arrange the data in order (For Odd no. of data)
+
+Median = Middle Value               #(For Odd no. of data)
+
+Data = 1,2,3,4,5
+Median = 3
+
+
+
+Median = Average of the Two middle values     #For Even no. of data  
+
+Data = 10,16,?,24,30
+
+Median = 20            #Middle Value
+
+
+
+
+#Mode : Whichever number occurred most in our data
+
+#1) No Mode:
+Data = 1,2,3,4,5
+Mode = No Mode
+
+#2) Bimodal
+
+Data = 1,1,2,3,3,4
+Mode = 1,2
+
+#3) MultiModal
+
+Data = 1,1,2,3,3,4,4
+Mode = 1,3,4
