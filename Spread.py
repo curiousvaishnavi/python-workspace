187b9b0fbae32afd27fe1f64d4ca29755d762a84
curiousvaishnavi <vaishnavidethe2810@gmail.com>
2026-08-17 12:21:51 +0530

    Spread

---
+++ Spread.py
@@ -0,0 +1,21 @@
+# Spread :- Tells the How far data is scattered from the centre point.
+
+# 1) Range :- Tells gap between the largest and smallest value in our data.
+
+Range = Largest Value - Smallest Value
+
+     Data = 10,20,30,50
+
+     Range = 50 - 10
+....Range = 40
+
+
+# 2)InterQuartile Range :- Works with the Central part of the data which does not contains the extreme values
+
+IQR = Q3 - Q1
+
+"""Q1 = 25 percentile
+     Q2 = 50 percentile (Median)
+     Q3 = 75 percentile
+     Q4 = 100 percentile"""
+
